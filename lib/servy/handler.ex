c6d90040032b625ecc40887915a30f9476079096
pedro siqueira <pedrowss@gmail.com>
2020-04-11 11:14:57 -0300

    alternate way to get absolute paths

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,7 +1,7 @@
 defmodule Servy.Handler do
   @moduledoc "Handles HTTP requests"
 
-  @pages_path Path.expand("../../pages", __DIR__)
+  @pages_path Path.expand("pages", File.cwd!())
 
   import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
   import Servy.Parser, only: [parse: 1]
