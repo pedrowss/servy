ac7b55bd5ea7382381023776798635c960af8eff
pedro siqueira <pedrowss@gmail.com>
2020-04-14 16:29:58 -0300

    Add render function

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -4,29 +4,28 @@ defmodule Servy.BearController do
 
   @templates_path Path.expand("templates", File.cwd!())
 
+  defp render(conv, template, bindings) do
+    content =
+      @templates_path
+      |> Path.join(template)
+      |> EEx.eval_file(bindings)
+
+    %{conv | status: 200, resp_body: content}
+  end
+
   def index(conv) do
     bears =
       Wildthings.list_bears()
       # |> Enum.filter(&Bear.is_grizzly/1)
       |> Enum.sort(&Bear.order_asc_by_name/2)
 
-    content =
-      @templates_path
-      |> Path.join("index.eex")
-      |> EEx.eval_file(bears: bears)
-
-    %{conv | status: 200, resp_body: content}
+    render(conv, "index.eex", bears: bears)
   end
 
   def show(conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
 
-    content =
-      @templates_path
-      |> Path.join("show.eex")
-      |> EEx.eval_file(bear: bear)
-
-    %{conv | status: 200, resp_body: content}
+    render(conv, "show.eex", bear: bear)
   end
 
   def create(conv, %{"name" => name, "type" => type}) do
