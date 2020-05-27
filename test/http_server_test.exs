fe98bf7b1bc06bfb098ffa1e760c22ba5d17c2d9
pedro siqueira <pedrowss@gmail.com>
2020-05-27 10:54:56 -0300

    Change HttpServerTest to send concurrent requests

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -2,27 +2,31 @@ defmodule HttpServerTest do
   use ExUnit.Case
 
   alias Servy.HttpServer
-  alias Servy.HttpClient
 
   test "accepts a request on a socket and sends back a response" do
     spawn(HttpServer, :start, [4000])
 
-    request = """
-    GET /wildthings HTTP/1.1\r
-    Host: example.com\r
-    User-Agent: ExampleBrowser/1.0\r
-    Accept: */*\r
-    \r
-    """
+    parent = self()
 
-    response = HttpClient.send_request(request)
+    max_concurrent_requests = 5
 
-    assert response == """
-           HTTP/1.1 200 OK\r
-           Content-Type: text/html\r
-           Content-Length: 20\r
-           \r
-           Bears, Lions, Tigers
-           """
+    # Spawn the client processes
+
+    for _ <- 1..max_concurrent_requests do
+      spawn(fn ->
+        # Send the request
+        {:ok, response} = HTTPoison.get("http://localhost:4000/wildthings")
+
+        send(parent, {:ok, response})
+      end)
+    end
+
+    for _ <- 1..max_concurrent_requests do
+      receive do
+        {:ok, response} ->
+          assert response.status_code == 200
+          assert response.body == "Bears, Lions, Tigers"
+      end
+    end
   end
 end
