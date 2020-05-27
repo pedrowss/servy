dc4a1a1655e4a3a3d998470e29572de20587f080
pedro siqueira <pedrowss@gmail.com>
2020-05-27 10:56:36 -0300

    Add comments

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -11,16 +11,17 @@ defmodule HttpServerTest do
     max_concurrent_requests = 5
 
     # Spawn the client processes
-
     for _ <- 1..max_concurrent_requests do
       spawn(fn ->
         # Send the request
         {:ok, response} = HTTPoison.get("http://localhost:4000/wildthings")
 
+        # Send the response back to the parent
         send(parent, {:ok, response})
       end)
     end
 
+    # Await all {:handled, response} messages from spawned processes
     for _ <- 1..max_concurrent_requests do
       receive do
         {:ok, response} ->
