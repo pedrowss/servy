733d8b71116f7ddd558671654888b6de43463ca5
pedro siqueira <pedrowss@gmail.com>
2020-05-29 17:07:34 -0300

    Return a maximum of three list elements (cache)

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -1,37 +1,44 @@
 defmodule Servy.PledgeServer do
   def listen_loop(state) do
-    IO.puts("\nWaiting for a message...")
-
     receive do
       {:create_pledge, name, amount} ->
         {:ok, _id} = send_pledge_to_service(name, amount)
-        new_state = [{name, amount} | state]
-        IO.puts("#{name} pledge #{amount}!")
-        IO.puts("New state is #{inspect(new_state)}")
+        most_recent_pledges = Enum.take(state, 2)
+        new_state = [{name, amount} | most_recent_pledges]
         listen_loop(new_state)
 
       {sender, :recent_pledges} ->
         send(sender, {:response, state})
-        IO.puts("Sent pledges to #{inspect(sender)}")
         listen_loop(state)
     end
   end
 
-  # def create_pledge(name, amount) do
-  #   {:ok, id} = send_pledge_to_service(name, amount)
+  def create_pledge(pid, name, amount) do
+    send(pid, {:create_pledge, name, amount})
+  end
 
-  #   # Cache the pledge:
-  #   [{"larry", 10}]
-  # end
+  def recent_pledges(pid) do
+    send(pid, {self(), :recent_pledges})
 
-  # def recent_pledges() do
-  #   # Returns the most recent pledges (cache):
-  #   [{"larry", 10}]
-  # end
+    receive do
+      {:response, pledges} -> pledges
+    end
+  end
 
   defp send_pledge_to_service(_name, _amount) do
     # Code goes here to send pledge to external service
-
     {:ok, "pledge-#{:rand.uniform(1_000)}"}
   end
 end
+
+alias Servy.PledgeServer
+
+pid = spawn(PledgeServer, :listen_loop, [[]])
+
+PledgeServer.create_pledge(pid, "larry", 10)
+PledgeServer.create_pledge(pid, "moe", 20)
+PledgeServer.create_pledge(pid, "curly", 30)
+PledgeServer.create_pledge(pid, "daisy", 40)
+PledgeServer.create_pledge(pid, "grace", 50)
+
+IO.inspect(PledgeServer.recent_pledges(pid))
